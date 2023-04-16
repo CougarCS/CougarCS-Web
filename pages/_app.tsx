9f6da54e6deb3cf675f90d9e0f06161a596caac4
Bryant Le <bryantleft@gmail.com>
2023-04-16 00:34:58 -0500

    feat: add App page component

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { AppProps } from "next/app";
+
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
