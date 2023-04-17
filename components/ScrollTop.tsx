e3bcf5dc57a54e8d9453c45f4e42be2d6ef1be4b
Minh Nguyen <vn.ndminh@gmail.com>
2023-04-17 13:30:40 -0500

    remove ScrollTop folder, fix indentation

---
+++ components/ScrollTop.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function ScrollTop() {
+  const router = useRouter();
+  const pathname: string = router.pathname;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return <></>;
+}
