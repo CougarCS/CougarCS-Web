ffa7bb331b94bae238546c7de8eb962ec2c0d7d8
Ashish Job <ashishjob104@gmail.com>
2023-04-21 14:01:10 -0500

    refactor: replaced single-quote with double-quote

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 type MetaDataProps = {
   title: string;
@@ -11,20 +11,20 @@ export const MetaData = ({ title, desc, url, img }: MetaDataProps) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta name='title' content={title} />
-      <meta name='description' content={desc} />
+      <meta name="title" content={title} />
+      <meta name="description" content={desc} />
 
-      <meta property='og:type' content='website' />
-      <meta property='og:url' content={url} />
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={desc} />
-      <meta property='og:image' content={img} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={desc} />
+      <meta property="og:image" content={img} />
 
-      <meta property='twitter:card' content='summary_large_image' />
-      <meta property='twitter:url' content={url} />
-      <meta property='twitter:title' content={title} />
-      <meta property='twitter:description' content={desc} />
-      <meta property='twitter:image' content={img} />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={desc} />
+      <meta property="twitter:image" content={img} />
     </Head>
   );
 };
