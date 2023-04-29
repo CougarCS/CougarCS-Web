82de7b9d7144ec6a77e90e1bdafe45fd68e6fff1
Thinh Ngo <ngothinh2511@gmail.com>
2023-04-29 14:04:15 -0500

    refactor: remove nav tags

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -2,10 +2,8 @@ import Link from "next/link";
 
 export const Socials = ({ href, children }) => {
   return (
-    <nav>
-      <Link href={href} target="_blank" rel="noopener nofollow">
-        {children}
-      </Link>
-    </nav>
+    <Link href={href} target="_blank" rel="noopener nofollow">
+      {children}
+    </Link>
   );
 };
