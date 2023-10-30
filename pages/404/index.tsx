a74e581da4be3903e5ef115802467286ae7fe0ce
yuryion <47436704+yuryion@users.noreply.github.com>
2023-10-29 20:10:20 -0500

    removed unnecessary asset import

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
-import error404 from "../../public/images/error404.svg";
 
 const FourOFour = () => {
 	return (
 		<div className="bg-[#C80F2E] h-screen flex justify-center items-center ">
 			<Image
-                src={error404}
-                alt="Page Not Found"
-            />
+				src="/images/error404.svg"
+				alt="Page Not Found"
+				width='500'
+				height='500'
+			/>
 		</div>
 	);
 };
