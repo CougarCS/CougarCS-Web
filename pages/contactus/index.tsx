ceeacd0251777dc7e64509905409f8d6c6bea516
Alan Johnson <alanjcd2004@gmail.com>
2023-11-01 17:47:37 -0500

    feat: add Contact Us page

---
+++ pages/contactus/index.tsx
@@ -0,0 +1,8 @@
+
+export default function ContactUs() {
+    return (
+        <div className="flex flex-col h-screen">
+            <h1 className="text-3xl font-bold underline">Contact Us</h1>
+        </div>
+    );
+}
