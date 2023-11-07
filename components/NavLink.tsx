5a9b18f6daabdefb272d8b37dea65a1ab9d396a0
Minh Nguyen <vn.ndminh@gmail.com>
2023-11-07 14:00:37 -0600

    feat: add a triangle under active link

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -9,12 +9,17 @@ interface NavLinkProps {
 export default function NavLink({ href, name }: NavLinkProps) {
   const pathname = usePathname();
 
-  return (
-    <Link
-      href={href}
-      className={pathname == href ? "text-red-700 font-medium" : ""}
-    >
+  return pathname === href ? (
+    <Link href={href} className="text-red-700 font-medium">
       {name}
+      <div
+        className="w-0 h-0 mx-auto 
+          border-l-[6px] border-l-transparent 
+          border-b-[8px] border-b-red-600 
+          border-r-[6px] border-r-transparent"
+      ></div>
     </Link>
+  ) : (
+    <Link href={href}>{name}</Link>
   );
 }
