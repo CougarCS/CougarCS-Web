95650990181673325947c0e987c701021a61b0c7
Brodypen <brodypen7987@gmail.com>
2023-12-22 00:22:00 -0600

    refactor: navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,10 @@ interface ActiveOption {
     | "About"
     | "Events"
     | "Gallery"
-    | "Branches"
+    | "Tutoring"
+    | "WebDev"
+    | "InfoSec"
+    // | "Branches"
     | "Contact Us"
     | "CodeRED"
     | "Register";
@@ -16,34 +19,39 @@ const NavBar = () => {
     { name: "About", href: "/" },
     { name: "Events", href: "/events" },
     { name: "Gallery", href: "/gallery" },
-    { name: "Branches", href: "/branches" },
+    // { name: "Branches", href: "/branches" },
+    { name: "Tutoring", href: "/tutoring"},
+    { name: "WebDev", href: "/webdev"},
+    { name: "InfoSec", href: "/infosec"},
     { name: "Contact Us", href: "/contactus" },
     { name: "CodeRED", href: "/codered" },
   ];
 
   return (
-    <nav className="flex flex-row justify-between mx-8 my-2 text-center">
-      <div className="relative">
-        <Link href="/" className="flex flex-row justify-between gap-4">
-          <img src="/icons/logo.svg" alt="Logo" className="mx-auto" />
-          <h1 className="my-1 font-extrabold uppercase text-2xl text-red-700">
-            CougarCS
-          </h1>
-        </Link>
-      </div>
-      <ul className="flex gap-4 my-2">
-        {navLinks.map((link) => (
-          <li key={link.name}>
-            <NavLink href={link.href} name={link.name} />
-          </li>
-        ))}
-        <li key="Register">
-          <Link className="bg-red-700 text-white px-4 py-2" href="/join">
-            Register
+    <header className="top-0 z-30">
+      <nav className="flex flex-row justify-between mx-8 my-2 text-center">
+        <div>
+          <Link href="/" className="flex flex-row justify-between gap-4">
+            <img src="/icons/logo.svg" alt="Logo" className="mx-auto" />
+            <h1 className="my-1 font-extrabold uppercase text-xl text-red-700">
+              CougarCS
+            </h1>
           </Link>
-        </li>
-      </ul>
-    </nav>
+        </div>
+        <ul className="flex gap-2 mt-2 text-xs">
+          {navLinks.map((link) => (
+            <li key={link.name}>
+              <NavLink href={link.href} name={link.name} />
+            </li>
+          ))}
+          <li key="Register">
+            <Link className="bg-red-700 text-white px-4 py-2" href="/join">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
